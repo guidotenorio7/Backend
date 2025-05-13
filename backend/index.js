4b8b41a037e7e50ee8408a237569de9a37ec6c16
Guido <guidotenorio08@gmail.com>
2025-05-13 18:51:31 -0300

    index

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,15 @@
 import express from 'express'
 import {controllers} from './controllers/controllers.js';
-import { logmiddleware } from './middlewares/log_middlewares.js';
 
 const app = express();
 
-// Middleware para decodificar JSON del body
 app.use(express.json());
-app.use(logmiddleware);
 
 controllers(app);
 
 const PORT = 3000;
-app.listen(
-    PORT,
-    ()=>{
+
+app.listen(PORT, ()=>{
         console.log(`servidor corriendo en http://localhost:${PORT}`);
     }
 );
