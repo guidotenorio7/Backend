f1146c0ba66e9b9c323a8e22ec3f023e3778da56
Guido <guidotenorio08@gmail.com>
2025-05-13 18:53:09 -0300

    del

--- backend/middlewares/log_middlewares.js
+++
@@ -1,8 +0,0 @@
-export function logmiddleware(req,res,next){
-    const date = (new date())
-    .toISOString()
-    .replace(/T/,'');
-    
-
-    console.log(`[${date}]${req.ip}${req.method}${req.url}`);
-}
