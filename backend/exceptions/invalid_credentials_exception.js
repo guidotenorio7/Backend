efd558e3cdd574d1819c9a652ef8f88937db183f
Guido <guidotenorio08@gmail.com>
2025-05-27 16:01:11 -0300

    credentials

--- backend/exceptions/invalid_credentials_exception.js
+++
@@ -1,6 +0,0 @@
-export class InvalidCredentialsException extends Error {
-    constructor(){
-        super('Credenciales invalidas.');
-        this.statusCode = 403;
-    }
-}
--- backend/exceptions/invalid_credentials_exceptions.js
+++ backend/exceptions/invalid_credentials_exceptions.js
@@ -1,6 +1,7 @@
 export class InvalidCredentialsException extends Error {
-    constructor(){
-        super('Credenciales invalidas.');
+    constructor() {
+        super('Credenciales inválidas.');
+        this.name = 'InvalidCredentialsException';
         this.statusCode = 403;
     }
 }
