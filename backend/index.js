859d769d7e64fa3826a7986f04830022c49d3506
Guido <guidotenorio08@gmail.com>
2025-05-27 16:02:53 -0300

    index and login

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,28 @@
 import express from 'express'
-import {controllers} from './controllers/controllers.js';
+import { controllers } from './controllers/controllers.js';
+import { errorHandlerMiddleware } from './middlewares/error_handler_middleware.js';
+import { logMiddleware } from './middlewares/log_middleware.js';
+import { addDependency } from './libs/dependencies.js';
+import { UserService } from './services/user.js';
+import { LoginService } from './services/login.js';
+import { UserMockup } from './mockups/user.js';
 
 const app = express();
 
-app.use(express.json());
+const router = express.Router();
+app.use('/api', router);
 
-controllers(app);
+router.use(express.json());
+router.use(logMiddleware);
+
+controllers(router);
+
+router.use(errorHandlerMiddleware);
+
+addDependency('UserService', UserService);
+addDependency('LoginService', LoginService);
+addDependency('UserModel', UserMockup);
+
 
 const PORT = 3000;
 
