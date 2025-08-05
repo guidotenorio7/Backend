9f8b6cb3be636a50d48bab6ea24b8551a9fe7dd0
Guido <guidotenorio08@gmail.com>
2025-08-04 21:15:46 -0300

    components src

---
+++ frontend/src/components/Menu.jsx
@@ -0,0 +1,13 @@
+export default function Menu() {
+  return (
+    <nav
+      className="menu"
+    >
+      <ul>
+        <li><a href="/">Inicio</a></li>
+        <li><a href="/about">Acerca de</a></li>
+        <li><a href="/contact">Contacto</a></li>
+      </ul>
+    </nav>
+  );
+}
