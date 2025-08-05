8760bce8cf46f16899da6daf68b894e2b738bc17
Guido <guidotenorio08@gmail.com>
2025-08-05 17:38:13 -0300

    Modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react';
 import './App.css';
 import Head from './components/Head.jsx';
 import Body from './components/Body.jsx';
+import Modal from './components/Modal.jsx';
 
 export default function App() {
   const [menuShowed, setMenuShowed] = useState(false);
 
   return (
     <>
+    <Modal></Modal>
       <Head
         setMenuShowed={setMenuShowed}
       />
---
+++ frontend/src/components/Modal.jsx
@@ -0,0 +1,8 @@
+export default function Modal() {
+    return <div className="modal-background">
+        <div className="modal-body">
+            
+        </div>
+        Este es modal
+    </div>
+}
