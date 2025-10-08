60700f81682c5163881e71005eefb824579f6715
Guido <guidotenorio08@gmail.com>
2025-10-08 13:15:12 -0300

    actualizacion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,45 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Head from './components/Head.jsx';
 import Body from './components/Body.jsx';
-import Modal from './components/Modal.jsx';
+import { ModalProvider } from './components/Modal.jsx';
+import { SnackbarProvider } from './components/Snackbar.jsx';
+import * as api from './libs/api.js';
+import { useSession } from './components/Session.jsx';
 
 export default function App() {
-  const [menuShowed, setMenuShowed] = useState(false);
+  const [menuShowed, setMenuShowed] = useState(true);
+    const session = useSession();
+
+  useEffect(() => {
+    const sessionText = localStorage.getItem('session');
+    if(!sessionText)
+      return;
+
+    const sessionData = JSON.parse(sessionText);
+
+    if (sessionData.token) {
+      api.headers.Authorization = `Bearer ${sessionData.token}`;
+      session.setIsLoggedIn(true);
+      session.setUser(sessionData.user);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <>
-    <Modal></Modal>
-      <Head
-        setMenuShowed={setMenuShowed}
-      />
-      <Body
-        menuShowed={menuShowed}
-      />
-    </>
+    <SnackbarProvider>
+      <ModalProvider>
+        <BrowserRouter>
+          <Head
+            setMenuShowed={setMenuShowed}
+          />
+          <Body
+              menuShowed={menuShowed}
+          />
+        </BrowserRouter>
+      </ModalProvider>
+    </SnackbarProvider>
   )
 }
